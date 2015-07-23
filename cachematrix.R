e8615f70365fe1c729a74791cd5455f3ce808fdf
Felipe Carvalho <felipe.carvalho07@gmail.com>
2015-07-23 19:43:41 -0400

    coursera project: this is my script to programming assignment

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+# makeCacheMatrix is awesome. It creates a list containing a function to:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    mt <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
+}
+
+# cacheSolve is awesome too! It is a function that returns the inverse of the matrix.
+# It checks if the inverse has already been computed.
+# If so, it gets the result and skips the computation.
+# If not, it computes the inverse, sets the value in the cache via setinverse function.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getmatrix()
+  if ( !is.null(m) ) {
+    message("getting cached data!")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
